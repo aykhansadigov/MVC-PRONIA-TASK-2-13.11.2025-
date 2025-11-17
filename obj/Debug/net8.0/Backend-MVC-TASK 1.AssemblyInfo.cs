53f6c08739f5f5526deb2abc1a127bdc61c891fc
Qurbanəli Sadıqov <gurbanalisadigov@gmail.com>
2025-11-17 17:28:05 +0400

    pronia inside

--- obj/Debug/net8.0/Backend-MVC-TASK 1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend-MVC-TASK 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend-MVC-TASK 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce7c86da35d8f55c5af746b6d50d71cc113fdf27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc4f0a8eb82ec7f9498677dc929e3bef57839f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend-MVC-TASK 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend-MVC-TASK 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
